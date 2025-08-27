ef4b8ba4625c9ab0866a3d9e4eea15bb3bc1e9e4
gabrieldiaz8 <gabidiaz4231@gmail.com>
2025-08-26 21:11:55 -0300

    Botones Inicio y Mis Compras

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  Product = []; // tu array completo
+  currentPage = 1;
+  itemsPerPage = 8;
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+  }
 }
