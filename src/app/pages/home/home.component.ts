3ece612061208ea50b99c1cafb809ad18306ff49
Lorenzo Gonzalez Meyer <gonzalez.lorenzo2311@gmail.com>
2025-08-26 22:14:53 -0300

    Navbar y Productos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-
 export class HomeComponent {
-  Product = []; // tu array completo
+  products = [
+    { id: 1, nombre: 'Botin', precio: 1500, imagen: 'assets/img/messi.jpg' },
+    { id: 2, nombre: 'Pelota', precio: 2000, imagen: 'assets/img/pelota.jpg' },
+    { id: 3, nombre: 'Remera', precio: 2500, imagen: 'assets/img/zanetti.webp' },
+    // ...más productos
+  ];
+
   currentPage = 1;
   itemsPerPage = 8;
+
+  get productosPaginados() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.products.slice(startIndex, startIndex + this.itemsPerPage);
+  }
 
   goToPage(page: number): void {
     this.currentPage = page;
