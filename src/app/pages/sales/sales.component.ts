e5680013a46b6c5a5f14c0f0e563bd211a616460
Tomas Gambino <tomigambino21@gmail.com>
2025-09-27 21:10:52 -0300

    Cambios en aspectos visuales de Mis ventas

--- src/app/pages/sales/sales.component.ts
+++ src/app/pages/sales/sales.component.ts
@@ -2,20 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SaleInterface } from '../../interfaces/sale-interface';
 import { ApiService } from '../../service/api.service';
+import { RouterModule } from '@angular/router';
 
-interface SaleTicket {
-  saleId: string;
-  date: string;
-  productName: string;
-  imageUrl: string;
-  unitPrice: number;
-  quantity: number;
-}
 
 @Component({
   selector: 'app-sales',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sales.component.html',
   styleUrls: ['./sales.component.css'],
 })
@@ -29,31 +22,7 @@ export class SalesComponent implements OnInit {
     this.sales = await this.loadSales()
   }
 
-  // Datos que luego vendrán del backend
-  items: SaleTicket[] = [
-    {
-      saleId: 'V-001',
-      date: '2025-09-10',
-      productName: 'Botines Nike Mercurial',
-      imageUrl: 'assets/img/messi.jpg',
-      unitPrice: 85000,
-      quantity: 12
-    },
-    {
-      saleId: 'V-002',
-      date: '2025-09-10',
-      productName: 'Mochila Jordan Essentials',
-      imageUrl: 'assets/img/mochila.jpg',
-      unitPrice: 35000,
-      quantity: 2
-    }
-  ];
-
-  trackById = (_: number, item: SaleTicket) => item.saleId;
-
-  get totalRevenue(): number {
-    return this.items.reduce((acc, r) => acc + r.unitPrice * r.quantity, 0);
-  }
+  trackById = (_: number, item: SaleInterface) => item.id;
 
   async loadSales(): Promise<SaleInterface[]>{
     return await this.apiService.getAllSales();
